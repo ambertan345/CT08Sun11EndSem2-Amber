aacbc89fffb6dccebc7936b8d2f89ffecf63817b
ambertan345 <ambertan2011@yahoo.com.sg>
2025-10-26 03:38:58 +0000

    Oct 26, 2025, 3:38 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,28 @@
+let bird
+let background, birdImg;
 
+function preload(){
+    // write code to preload bird.png and background.png
+    // refer to assets folder
+    birdImg = loadImage("assets/bird.png")
+    background = loadImage("assets/background.png")
+    
+}
+
+function setup(){
+    new Canvas(400, 600);
+    // write code to create sprite and load bird image on sprite
+    bird = new Sprite();
+    bird.x = width/2;
+    bird.y = height/2;
+
+    background = new Sprite();
+    background.x = width/2;
+    background.y = height/2;
+    background.layer = 0;
+
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,25 @@
+let square;
 
+function setup(){
+    new Canvas(400, 600);
+   
+    // code to set the gravity of the world
+    world.gravity.y = 10;
+    // code to create a new square sprite
+    square = new Sprite();
+    square.width = 50;
+    square.height = 50;
+    square.x = width/2;
+    square.y = height/2;
+    square.color = "blue";
+}
+
+function draw(){
+    background("grey");
+
+    // code to check if mouse is pressed i.e. mouse.presses()
+        // if mouse is pressed, the square sprite "flies" up
+    if (mouse.presses()) {
+        square.vel.y = -6;
+    }
+}
